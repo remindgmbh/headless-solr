99f30b5dfeed7584e81e5886e9a7638f07c7271e
David Mellen <d.mellen@remind.de>
2022-10-21 07:48:04 +0200

    Initial commit

---
+++ Configuration/TCA/Overrides/sys_template.php
@@ -0,0 +1,11 @@
+<?php
+
+defined('TYPO3_MODE') || die;
+
+use TYPO3\CMS\Core\Utility\ExtensionManagementUtility;
+
+ExtensionManagementUtility::addStaticFile(
+    'rmnd_headless_solr',
+    'Configuration/TypoScript',
+    'REMIND - Headless Solr'
+);
