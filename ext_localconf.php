b95faa07d4c1f1e929fae99c3b3c124e9173bd3c
David Mellen <d.mellen@remind.de>
2023-05-03 11:28:47 +0200

    Change deprecated TYPO3_MODE constant
    RHSTE-1

--- ext_localconf.php
+++ ext_localconf.php
@@ -5,7 +5,7 @@ declare(strict_types=1);
 use ApacheSolrForTypo3\Solr\Controller\SearchController;
 use Remind\HeadlessSolr\Controller\SearchController as HeadlessSearchController;
 
-defined('TYPO3_MODE') || die('Access denied.');
+defined('TYPO3') || die('Access denied.');
 
 (function () {
     $GLOBALS['TYPO3_CONF_VARS']['SYS']['Objects'][SearchController::class] = [
