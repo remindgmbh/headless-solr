133e78b3e4f78624eaaa2e6645db5381b5eeaeaf
David Mellen <d.mellen@remind.de>
2025-03-05 14:44:19 +0100

    feat!: make extension TYPO3 13 compatible

--- Classes/Controller/SearchController.php
+++ Classes/Controller/SearchController.php
@@ -134,7 +134,12 @@ class SearchController extends BaseSearchController
             $documents[] = $event->getDocument();
         }
 
-        $paginationResult = $this->jsonService?->serializePagination($pagination, 'page', $currentPage);
+        $paginationResult = $this->jsonService?->serializePagination(
+            $this->uriBuilder,
+            $pagination,
+            'page',
+            $currentPage
+        );
 
         $count = $searchResultSet->getAllResultCount();
 
